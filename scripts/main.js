f251c64035903f73777da5486affdfdc099f89e6
Sean Canning <seancanning@outlook.com>
2026-07-20 15:45:58 +0100

    - hid audio control behind video - added pausing logic

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,6 @@ async function LoadMedia() {
         const response = await fetch("media.json");
         const data = await response.json();
         videoSrc = data["media"][0];
-        console.log(videoSrc);
 
         document.getElementById("bg-video").src = `media/${videoSrc}`;
     }
@@ -21,6 +20,7 @@ async function LoadMedia() {
 // 🕑 Timings 🕑
 var fadeInTime = 5, fadeOutTime = 5;
 var fadeInterval = 20, songInterval = 3;
+var fading;
 
 // 🎵 JSON Playlist 🎵
 // Fetch the song list asynchronously
@@ -60,19 +60,26 @@ window.addEventListener('load', () => {
 window.addEventListener('DOMContentLoaded', () => {
     const audio = document.getElementById("bg-music");
 
-    // Audio Paused Listener
+    // Paused Listener
+    var truly_paused = false;
     audio.addEventListener('pause', () => {
-        console.log("Music paused!");
+        audio.play();
+        if (fading) { clearInterval(fading); }
+        fadeOut(audio, fadeOutTime * 400);
+        truly_paused = truly_paused == false ? true : false;
     });
 
-    // Fade-In Listener
+    // Play (Fade-In) Listener
     audio.addEventListener('play', () => {
-        if (audio.currentTime == 0) { fadeIn(audio, fadeInTime * 1000); }
+        if (!truly_paused) { fadeIn(audio, fadeInTime * 1000); }
     });
 
-    // Fade-Out Listener
+    // End of Song (Fade-Out) Listener
     audio.addEventListener('timeupdate', () => {
         let timeRemaining = audio.duration - audio.currentTime;
+        console.log("ad: " + audio.duration);
+        console.log("ac: " + audio.currentTime);
+        console.log("tr: " + timeRemaining);
 
         if (timeRemaining <= fadeOutTime && !audio.dataset.fadingOut) {
             audio.dataset.fadingOut = "true"; // Flag set
@@ -97,11 +104,9 @@ function AutoNextSong(audioElement) {
 
 // Fade-In Handler
 function fadeIn(audioElement, duration) {
-    audioElement.volume = 0;
-
     let step = fadeInterval / duration;
 
-    let fade = setInterval(() => {
+    fading = setInterval(() => {
         if (audioElement.volume + step >= 1) {
             audioElement.volume = 1;
             clearInterval(fade);
@@ -116,9 +121,7 @@ function fadeOut(audioElement, duration = 2000, onComplete) {
 
     let fade = setInterval(() => {
         if (audioElement.volume - step <= 0) {
-            audioElement.volume = 0;
-            audioElement.pause();
-
+            audioElement.volume = 0
             clearInterval(fade);
 
             onComplete();
